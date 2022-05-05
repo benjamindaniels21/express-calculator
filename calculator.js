4335d07954dd82ea0dfeab00b179f39938d420be
Ben Daniels <benjamindaniels21@gmail.com>
2022-05-05 09:41:15 -0400

    finished calculator that adds two numbers on the server

--- calculator.js
+++ calculator.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
+  let sum = num1 + num2;
+
+  res.send(`The total is ${sum}!`);
 });
 
 app.listen(3000, function () {
